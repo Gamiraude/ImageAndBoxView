b154e65f3755cb65de88d6d66204c71f2ed15777
DESKTOP-99CVDAU\User_12 <gamiraude@gmail.com>
2022-02-08 19:51:25 +0100

    Add project files.

---
+++ Image/Image/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Image
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LogoImage.Source = ImageSource.FromResource("Image.Images.logo.png", typeof(MainPage).Assembly);
+        }
+    }
+}
